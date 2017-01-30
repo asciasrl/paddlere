e18e8af0f21afe97f690a2eb2d5daa32fc5ff565
Sergio su xubuntu su copper <sergio@ascia.it>
2017-01-30 22:52:49 +0100

    WIP timeline

--- symfony/src/AppBundle/Controller/DefaultController.php
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -15,6 +17,44 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @Route("timeline", name="timeline")
+     */
+    public function timelineAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->setMethod('GET')
+            ->add('begin', DateType::class, array(
+                'html5' => true,
+                'data' => (new \DateTime())->sub(new \DateInterval('P1M'))
+            ))
+            ->add('interval', DateIntervalType::class, array(
+                'placeholder' => array('days' => 'Days'),
+                'with_years'  => false,
+                'with_months' => false,
+                'with_days'   => true,
+                'data' => new \DateInterval('P31D'),
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            dump($data);
+
+            // ... perform some action, such as saving the data to the database
+
+        }
+
+        return $this->render('default/timeline.html.twig', [
+            'form' => $form->createView(),
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
