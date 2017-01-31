de75c40e7e347670967e6086f6248c8108ad85f7
Sergio su xubuntu su copper <sergio@ascia.it>
2017-01-31 18:36:14 +0100

    WIP timeline google

--- symfony/src/AppBundle/Controller/DefaultController.php
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -2,10 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\Common\Collections\Criteria;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -28,10 +32,10 @@ class DefaultController extends Controller
     public function timelineAction(Request $request)
     {
         $form = $this->createFormBuilder()
-            ->setMethod('GET')
+            //->setMethod('GET')
             ->add('begin', DateType::class, array(
                 'html5' => true,
-                'data' => (new \DateTime())->sub(new \DateInterval('P1M'))
+                'data' => (new \DateTime())->sub(new \DateInterval('P1M'))->add(new \DateInterval('P1D'))
             ))
             ->add('interval', DateIntervalType::class, array(
                 'placeholder' => array('days' => 'Days'),
@@ -40,6 +44,11 @@ class DefaultController extends Controller
                 'with_days'   => true,
                 'data' => new \DateInterval('P31D'),
             ))
+            ->add('field', ChoiceType::class, array(
+                'choices' => array('campo 1' => 'campo 1','campo 2' => 'campo 2','campo 3' => 'campo 3')
+            ))
+            ->add('dummy', CheckboxType::class, array('required' => false))
+            ->add('Show', SubmitType::class)
             ->getForm();
 
         $form->handleRequest($request);
@@ -49,12 +58,30 @@ class DefaultController extends Controller
 
             dump($data);
 
-            // ... perform some action, such as saving the data to the database
+            $criteria = new Criteria();
+            $criteria->where($criteria->expr()->neq('inizio', null));
+            if ($data['dummy']) {
+                $criteria->andWhere($criteria->expr()->orX(
+                    $criteria->expr()->eq('campo',$data['field']),
+                    $criteria->expr()->eq('evento','Dummy')));
+            } else {
+                $criteria->andWhere($criteria->expr()->eq('campo',$data['field']));
+            }
+            $criteria->andWhere($criteria->expr()->gte('dataora',$data['begin']));
+            $criteria->andWhere($criteria->expr()->lte('dataora',(clone $data['begin'])->add($data['interval'])));
+            $criteria->orderBy(array('inizio' => 'ASC'));
 
+            $events = $this->getDoctrine()->getRepository('AppBundle:BorghesianaLog')->matching($criteria);
+
+            dump($events);
+
+        } else {
+            $events = array();
         }
 
         return $this->render('default/timeline.html.twig', [
             'form' => $form->createView(),
+            'events' => $events,
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
