6a6a7394c15c73a8ad535157c9428cc3758026c5
Sergio su xubuntu su copper <sergio@ascia.it>
2017-10-19 12:01:43 +0200

    Route annotation

--- symfony/src/PaddlereBundle/Controller/DefaultController.php
+++ symfony/src/PaddlereBundle/Controller/DefaultController.php
@@ -22,6 +22,7 @@ use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
